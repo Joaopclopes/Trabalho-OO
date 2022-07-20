ef475112de4d5ba685aec756515996f662da72ee
Pedro Alexandre <104382913+PedroAlexx@users.noreply.github.com>
2022-07-20 12:26:58 -0300

    Update Tabuleiro.java

--- Dama/src/main/java/com/mycompany/dama/Tabuleiro.java
+++ Dama/src/main/java/com/mycompany/dama/Tabuleiro.java
@@ -1,16 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.dama;
 
+
+import javax.swing.JFrame;
 /**
  *
  * @author ice
  */
-public class Tabuleiro 
+public class Tabuleiro
 {
     int dimensao = 8;
+    
     
     private static final String Alfabeto = "0ABCDEFGH"; // util para as coordenadas
     
