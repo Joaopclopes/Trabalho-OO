eb861b732deae004d932f2bf945329ac741c59e1
Pedro Alexandre <104382913+PedroAlexx@users.noreply.github.com>
2022-07-31 13:24:25 -0300

    atualizando peca com a função de inserir icon

--- Dama/src/main/java/dama/model/Peca.java
+++ Dama/src/main/java/dama/model/Peca.java
@@ -1,8 +1,13 @@
 package dama.model;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
 public abstract class Peca extends Casa
 {
     Posicao posicao;
+    
+    
     
     public Peca()
     {
@@ -10,6 +15,13 @@ public abstract class Peca extends Casa
     }
     
     public Posicao getPosicao(){return posicao;};
+    public void InserePeca(int i, int j){
+        if(getCasa(i,j).getcomPeca())
+                {
+                    Icon icon = new ImageIcon("pecapreta.png");
+                    getCasa(i,j).setIcon(icon);
+                }
+    }
     
     abstract void MovePeca(Posicao Destino);
     abstract boolean validaPosicao(Posicao pos);
